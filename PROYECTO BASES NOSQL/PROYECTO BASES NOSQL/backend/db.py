5ddad6c46157fc15c309b2a20ec9c765b00e101c
JavierPizarroFleming6517 <javier.pizarro@alumnos.ucn.cl>
2025-05-12 17:15:56 -0400

    Delete PROYECTO BASES NOSQL directory

--- PROYECTO BASES NOSQL/PROYECTO BASES NOSQL/backend/db.py
+++
@@ -1,8 +0,0 @@
-import plyvel
-import os
-
-# Asegurarse de que el directorio exista
-os.makedirs('/data/leveldb', exist_ok=True)
-
-# Crear o abrir la base de datos en modo de escritura
-rocksdb_client = plyvel.DB('/data/leveldb', create_if_missing=True)
